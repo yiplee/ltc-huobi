fa2f720d16fb8faeb47d1f32d441d76a3f243f74
yiplee <guoyinl@gmail.com>
2017-06-05 01:04:41 +0800

    spider

---
+++ ltc_huobi/spider.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import http.client
+from ltc.models import Record
+import json
+
+def get_current_price():
+    conn = http.client.HTTPSConnection('api.huobi.com')
+    conn.request('GET','/staticmarket/ticker_ltc_json.js')
+    r = conn.getResponse()
+    if r.status == 200 :
+        data = r.read()
+        string = data.decode('utf8').replace("'", '"')
+        json_data = json.loads(string)
+        json = json.dumps(json_data,indent=4, sort_keys=True)
+
+        price = json['ticker']['last']
+        date = json['time']
+
+        recod = Record.create(price,date)
+        record.save()
+
+
+if __name__ == '__main__':
+    get_current_price()
