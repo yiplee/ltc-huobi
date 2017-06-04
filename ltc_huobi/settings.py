54cb06bb2e6dd3f64774f1e91b2bf0b8fef8c3b4
yiplee <guoyinl@gmail.com>
2017-06-04 23:12:47 +0800

    move static files

--- ltc_huobi/settings.py
+++ ltc_huobi/settings.py
@@ -117,5 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
