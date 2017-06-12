9d511d5222845b1941d0eb7aebee88595166932b
yiplee <guoyinl@gmail.com>
2017-06-13 00:36:46 +0800

    update spider

--- spider.py
+++ spider.py
@@ -3,16 +3,21 @@
 import http.client
 from ltc.models import Record
 import json
+import time
 
 conn = http.client.HTTPSConnection('api.huobi.com')
 conn.request('GET','/staticmarket/ticker_ltc_json.js')
-r = conn.getresponse()
-if r.status == 200 :
-    data = r.read()
-    string = data.decode('utf8').replace("'", '"')
-    json_data = json.loads(string)
-    ticker = json_data['ticker']
-    price = ticker['last']
-    date = int(json_data['time'])
-    record = Record.create(price,date)
-    record.save()
+while True:
+    r = conn.getresponse()
+    if r.status == 200 :
+        data = r.read()
+        string = data.decode('utf8').replace("'", '"')
+        json_data = json.loads(string)
+        print json_data
+        ticker = json_data['ticker']
+        price = ticker['last']
+        date = int(json_data['time'])
+        record = Record.create(price,date)
+        record.save()
+
+    time.sleep(5)
