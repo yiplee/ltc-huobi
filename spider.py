e0a4d0ac3a3ee2758e52a017f1b54734071f09b9
yiplee <guoyinl@gmail.com>
2017-06-13 14:50:02 +0800

    add request header

--- spider.py
+++ spider.py
@@ -7,7 +7,9 @@ import time
 
 while True:
     conn = http.client.HTTPSConnection('api.huobi.com')
-    conn.request('GET','/staticmarket/ticker_ltc_json.js')
+    body = {}
+    headers = {'Content-type': 'application/json', 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'}
+    conn.request('GET','/staticmarket/ticker_ltc_json.js', json.dumps(body),headers)
     r = conn.getresponse()
     if r.status == 200 :
         data = r.read()
@@ -16,10 +18,9 @@ while True:
         ticker = json_data['ticker']
         price = ticker['last']
         date = int(json_data['time'])
-
         latest_record = Record.objects.latest
         if date > latest_record.timestamp:
             record = Record.create(price,date)
             record.save()
-
+    conn.close()
     time.sleep(5)
