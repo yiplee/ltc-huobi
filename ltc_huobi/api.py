12fa0b8f6ddf198f163959c3922b9178156402f7
yiplee <guoyinl@gmail.com>
2017-07-04 00:54:51 +0800

    add active

--- ltc_huobi/api.py
+++ ltc_huobi/api.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.db.models import Max
 from django.db.models import Min
+from django.db.models import count
+from django.db.models import Sum
 import datetime
 import time
 
@@ -13,11 +15,17 @@ from ltc.models import Record
 def get_ltc_price(request):
     date = datetime.date.today()
     today = time.mktime(date.timetuple()) + 16 * 60 * 60
-    objects = Record.objects.filter(timestamp__gte = today)
-    arg = objects.aggregate(Max('price'),Min('price'))
+    objects = Record.objects.filter(timestamp__gte = today).order_by('timestamp')
+    arg = objects.aggregate(Max('price'),Min('price'),Count('entry'))
+    count = 20
+    if arg["entry__count"] < count:
+        count = arg["entry__count"]
 
-    record = objects.latest()
-    start = objects.earliest()
+    latest_objects = objects[:count]
+    sum = latest_objects.aggregate(Sum('count'))
+    active = count / sum
+    record = objects.last()
+    start = objects.first()
     if (record):
         record = record.dumpJSON()
         json = {"latest" : record}
@@ -25,6 +33,7 @@ def get_ltc_price(request):
         json["low"] = arg['price__min']
         # json["today"] = today
         json["open"] = start.price
+        json["active"] = active
         return JsonResponse(json)
     else:
         return HttpResponse('No Record')
