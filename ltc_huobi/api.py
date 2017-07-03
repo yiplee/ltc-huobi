e79e4f9dfe99923d8a82430f9f988d758ea92561
yiplee <guoyinl@gmail.com>
2017-07-04 00:34:17 +0800

    start

--- ltc_huobi/api.py
+++ ltc_huobi/api.py
@@ -11,19 +11,20 @@ from ltc.models import Record
 
 # 数据库操作
 def get_ltc_price(request):
-    record = Record.objects.latest()
-
     date = datetime.date.today()
     today = time.mktime(date.timetuple()) + 16 * 60 * 60
     objects = Record.objects.filter(timestamp__gte = today)
     arg = objects.aggregate(Max('price'),Min('price'))
 
+    record = objects.latest()
+    start = objects.earliest()
     if (record):
         record = record.dumpJSON()
         json = {"record" : record}
         json["max"] = arg['price__max']
         json["min"] = arg['price__min']
-        json["today"] = today
+        # json["today"] = today
+        json["start"] = start.price
         return JsonResponse(json)
     else:
         return HttpResponse('No Record')
